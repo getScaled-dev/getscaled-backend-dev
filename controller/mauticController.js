673b5658113c01eac220f44bc0e24ea34cb10417
getScaled-dev <tayyab@getscaled.com>
2023-06-06 01:02:40 +0500

    option source filters

--- controller/mauticController.js
+++ controller/mauticController.js
@@ -25,8 +25,42 @@ const getContacts = async (req, res) => {
         });
 };
 
+const createContact = async (req, res) => {
 
 
+    try {
+        // Find the document by ID
+        const doc = await LinkedinData2.findById(id);
+
+        if (!doc) {
+            return res.status(404).json({ error: 'Document not found' });
+        }
+
+        // Update the document with the new data
+        doc.firstName = req.body.firstName || doc.firstName;
+        doc.lastName = req.body.lastName || doc.lastName;
+        doc.companyPhone = req.body.companyPhone || doc.companyPhone;
+        doc.mobilePhone = req.body.mobilePhone || doc.mobilePhone;
+        doc.companyName = req.body.companyName || doc.companyName;
+        doc.jobTitle = req.body.jobTitle || doc.jobTitle;
+        doc.dob = req.body.dob || doc.dob;
+        doc.address = req.body.address || doc.address;
+        doc.address2 = req.body.address2 || doc.address2;
+        doc.city = req.body.city || doc.city;
+        doc.state = req.body.state || doc.state;
+        doc.zip = req.body.zip || doc.zip;
+        doc.age = req.body.age || doc.age;
+        doc.email = req.body.email || doc.email;
+
+        // Save the updated document
+        const updatedDoc = await doc.save();
+
+        res.json(updatedDoc);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
 
 
 module.exports = {
